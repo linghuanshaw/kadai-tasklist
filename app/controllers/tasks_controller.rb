27be098f4629112832f36d25e4cb7d4fff6aa07d
reikan shou <linghuanshaw@yahoo.co.jp>
2017-04-19 00:27:32 +0000

    database check

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
    def index
-      @tasks=Task.all
+      @tasks=Task.order(created_at: :desc).page(params[:page]).per(10)
    end
    
    def show
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
    private
    
    def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content,:title,:status)
    end
    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(title:'title1',content:'content1',status:'status1')
+Task.create(title:'title2',content:'content2',status:'status2')
+Task.create(title:'title3',content:'content3',status:'status3')
+Task.create(title:'title4',content:'content4',status:'status4')
+Task.create(title:'title5',content:'content5',status:'status5')
+
+(1..100).each do |number|
+   Task.create(title:'title'+number.to_s,content:'content'+number.to_s,status:'status'+number.to_s)
+end
+
+   
+   
