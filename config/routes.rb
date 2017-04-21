8d3f335796c7a1578d9f58fa21451340d2869f9d
linghuanshaw <linghuanshaw@gmail.com>
2017-04-21 17:23:27 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   
   get 'newtask', to: 'tasks#new'
-  resources :tasks
+  resources :tasks,  only: [:index, :show, :new, :create]
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
